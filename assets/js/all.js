1a070d047ec9104d79cdc27b5aa5acb1cb5b1926
JasonWang <ff1986755087@gmail.com>
2021-04-09 22:31:01 +0800

    Update 2021-04-09T14:30:57.011Z

---
+++ assets/js/all.js
@@ -0,0 +1,90 @@
+"use strict";
+
+new Vue({
+  el: '#banner',
+  data: {
+    bannerItem: [{
+      imgSrc: '../assets/images/banner.jpg'
+    }, {
+      imgSrc: '../assets/images/banner.jpg'
+    }]
+  }
+});
+new Vue({
+  el: '#search',
+  data: {
+    searchKey: ''
+  }
+});
+new Vue({
+  el: '#topMenu',
+  data: {
+    active: 0,
+    menu: ['熱銷排行TOP10', '優惠活動', '名人堂', '紅茶系列', '綠茶系列', '鮮奶茶系列', '水果茶系列', '珍珠奶茶系列', '抹茶系列']
+  }
+});
+new Vue({
+  el: '#top-10',
+  data: {
+    dispalay: 2,
+    tea: [{
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }, {
+      name: 'tea1'
+    }]
+  },
+  methods: {
+    load: function load() {
+      var teaLen = this.tea.length; // console.log('t :>> ', t)
+
+      if (this.dispalay < teaLen) {
+        this.dispalay += 2;
+      } else {
+        alert('nodata');
+      }
+    }
+  }
+});
+//# sourceMappingURL=all.js.map
