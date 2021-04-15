286f9c93706bc410e9dfe80b578fe2bef5801718
JasonWang <ff1986755087@gmail.com>
2021-04-16 01:37:26 +0800

    Update 2021-04-15T17:37:21.441Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,14 +1,27 @@
 "use strict";
 
+var data_share = {
+  popuphide: true,
+  //控制 fixedBg 狀態
+  free_open_hide: true,
+  //控制 free_open 狀態
+  free_ok_hide: true,
+  //控制 free_ok 狀態
+  free_drink_hide: true,
+  //控制 free_drink 狀態
+  drink_detail_hide: true //控制 drink_detail 狀態
+
+};
 new Vue({
   el: '#search',
-  data: {}
+  data: {
+    data_share: data_share
+  }
 });
 new Vue({
   el: '#main',
   data: {
-    popuphide: true //控制popup 狀態
-
+    data_share: data_share
   },
   computed: {},
   methods: {}
