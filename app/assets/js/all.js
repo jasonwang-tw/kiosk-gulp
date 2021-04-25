be908999e64ce9e6d8f1278ef4ed19b412c930cb
JasonWang <ff1986755087@gmail.com>
2021-04-26 01:04:25 +0800

    補齊搜尋&購物車資料畫面

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,6 +2,7 @@ let share = {
   popuphide: true, //控制 fixedBg 狀態
   free_open_hide: true, //控制 free_open 狀態
   free_ok_hide: true, //控制 free_ok 狀態
+  free_error_hide: true, //控制 free_ok 狀態
   free_drink_hide: true, //控制 free_drink 狀態
   drink_detail_hide: true, //控制 drink_detail 狀態
   // order status
